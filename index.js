4af8face7ac5d1b1ed9b78e386011a71790f0ac4
Abhishek Dhapare <abhishek.dhapare@gmail.com>
2022-03-12 16:25:38 +0530

    MongoDB Connection, Scheme, API: get all, get single, create, delete, update

--- index.js
+++ index.js
@@ -1,32 +1,98 @@
 const express = require("express");
+require("dotenv").config();
+const Blog = require("./models/Blog");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello From NodeJS World!");
 });
 
-const blogs = [
-  {
-    id: "1",
-    title: "Blog Title 1",
-    body: "Blog 1 Body",
-  },
-  {
-    id: "2",
-    title: "Blog Title 2",
-    body: "Blog 2 Body",
-  },
-  {
-    id: "3",
-    title: "Blog Title 3",
-    body: "Blog 3 Body",
-  },
-];
-app.get("/api/v1/blogs", (req, res) => {
-  res.status(200).json(blogs);
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, () => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/api/v1/blogs", async (req, res) => {
+  try {
+    const blogs = Blog.find({});
+    res.status(200).json({
+      status: 200,
+      error: false,
+      data: blogs,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.listen(3000, () => {
-  console.log(`Server is runing on port 3000`);
+app.post("/api/v1/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save();
+  res.status(201).json(blog);
+});
+
+app.get(`/api/v1/blogs/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blog = await Blog.find({ _id: id });
+    res.status(200).json({
+      status: 200,
+      error: false,
+      data: blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Bad Request",
+    });
+  }
+});
+
+app.patch(`/api/v1/blogs/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedBlog = await Blog.updateOne(
+      { _id: id },
+      { $set: { title: req.body.title, body: req.body.body } },
+    );
+
+    res.status(200).json({
+      status: 200,
+      error: false,
+      message: "Blog Updated Successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Bad Request",
+    });
+  }
+});
+
+app.delete(`/api/v1/blogs/:id`, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteBlog = await Blog.deleteOne({ _id: id });
+
+    res.status(200).json({
+      status: 200,
+      error: false,
+      message: "Blog Deleted Successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Bad Request",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is runing on port ${PORT}`);
 });
---
+++ models/Blog.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
